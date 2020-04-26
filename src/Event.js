25452f055e04b2287a99b58f26bcab715ff5dadc
TetaTeuta <teuta.stazic@gmail.com>
2020-04-26 18:20:24 +0200

    4.4 final adjustments

--- src/Event.js
+++ src/Event.js
@@ -17,12 +17,14 @@ class Event extends Component {
       <div className="event">
         <div className="event__Overview">
           <p className="event__Overview--name">{this.props.event.name}</p>
-          <p className="event__Overview--localDate">{this.props.event.local_date}</p>
+          <p className="event__Overview--localDate">{this.props.event.local_date} | {this.props.event.local_time}</p>
           <button onClick={() => this.handleShowDetails()}>show details</button>
         </div>
         {showDetails &&
           <div className="event__Details">
-            <p className="event__Details--description">{this.props.event.description}</p>
+            <h3>About event:</h3>
+            <h4><a href={this.props.event.link} target="_blank">GoTo MeetUp</a></h4>
+            <p className="event__Details--description" dangerouslySetInnerHTML={{ __html: this.props.event.description }} />
           </div>
         }
       </div>
