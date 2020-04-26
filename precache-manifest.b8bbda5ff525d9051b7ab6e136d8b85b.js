d029f8f71f7fc9a1509025808b2e525115bf58b9
TetaTeuta <teuta.stazic@gmail.com>
2020-04-26 13:19:49 +0200

    Updates

---
+++ precache-manifest.b8bbda5ff525d9051b7ab6e136d8b85b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a98ff6247203bd53ea2f2c0bb6235b3",
+    "url": "/meetup22/index.html"
+  },
+  {
+    "revision": "17c0169b689a2ad70283",
+    "url": "/meetup22/static/css/main.029a4330.chunk.css"
+  },
+  {
+    "revision": "a20fce37dec216b11159",
+    "url": "/meetup22/static/js/2.2f417d1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meetup22/static/js/2.2f417d1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17c0169b689a2ad70283",
+    "url": "/meetup22/static/js/main.2dcaf7f0.chunk.js"
+  },
+  {
+    "revision": "034443e6e3ef2e86c808",
+    "url": "/meetup22/static/js/runtime-main.0b988785.js"
+  }
+]);
