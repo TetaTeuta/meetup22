b5cd95557033a8d8b667b7022622ad454d2b1bb4
TetaTeuta <teuta.stazic@gmail.com>
2020-04-30 17:54:00 +0200

    Updates

--- precache-manifest.2b623ea54ac5c0e321bd077a8a34ee4c.js
+++ precache-manifest.2b623ea54ac5c0e321bd077a8a34ee4c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "6d13e0d0d8ce59b46a978308e01eb390",
+    "revision": "8e9ada4332b4a90b9f0445642b620417",
     "url": "/meetup22/index.html"
   },
   {
-    "revision": "cdc3014ad69d202de3e3",
+    "revision": "90ab930766539b14dfd3",
     "url": "/meetup22/static/css/main.aca22411.chunk.css"
   },
   {
-    "revision": "64ae953b6d9a9b3f45e3",
-    "url": "/meetup22/static/js/2.3654e68b.chunk.js"
+    "revision": "593db289094007325a68",
+    "url": "/meetup22/static/js/2.cfc8ed27.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/meetup22/static/js/2.3654e68b.chunk.js.LICENSE.txt"
+    "url": "/meetup22/static/js/2.cfc8ed27.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cdc3014ad69d202de3e3",
-    "url": "/meetup22/static/js/main.878e818d.chunk.js"
+    "revision": "90ab930766539b14dfd3",
+    "url": "/meetup22/static/js/main.1b1462d3.chunk.js"
   },
   {
     "revision": "034443e6e3ef2e86c808",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup22/precache-manifest.7723ba835890402aac9cbd098b5e4e8d.js"
+  "/meetup22/precache-manifest.2b623ea54ac5c0e321bd077a8a34ee4c.js"
 );
 
 self.addEventListener('message', (event) => {
